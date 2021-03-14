dafed4aa5f47e35749b04c3e60eb4343f0a2027d
thr0m3l <1705069@ugrad.cse.buet.ac.bd>
2021-03-14 07:39:00 +0600

    Handled corner cases on 2.1

--- Offline 2/1705069_1.asm
+++ Offline 2/1705069_1.asm
@@ -36,28 +36,41 @@ MAIN PROC
     
     MOV AL, A
     CMP AL, B ;Compare A with B
+    JE CASE3    ; if A = B
+    CMP AL, C; Compare A with C
+    JE CASE4 ; if A = C
+    MOV AL, B
+    CMP AL, C ; Compare B with C
+    JE CASE5 ; if B = C
+    
+    MOV AL, A
+    CMP AL, B
     JG CASE1    ; if A > B
     JL CASE2    ; if A < B
-    JE CASE3    ; if A = B
+    
+    
 
     CASE1: ;if A>B
+        
+        MOV AL, A
         CMP AL, C ;Compare A with C
-        JLE ANSA ;if A <= C
+        JL ANSA ;if A < C
         
         MOV AL, B
         CMP AL, C ;Compare B with C
-        JGE ANSB   ;if B >= C
+        JG ANSB   ;if B > C
             
         JMP ANSC ; else the answer is C
         
     CASE2: ;if A<B
         CMP AL, C ;Compare A with C
-        JGE ANSA ; if A >= C
+        JG ANSA ; if A > C
         
         MOV AL, C
         CMP AL, B ;Compare C with B
-        JGE ANSB ; if C >= B
+        JG ANSB ; if C > B
         
+        JMP ANSC; else the answer is C
         
         
     CASE3: ;if A=B
@@ -66,7 +79,20 @@ MAIN PROC
         JG ANSC
         JL ANSA
         
+    CASE4: ;if A=C
+        
+        MOV AL, A
+        CMP AL, B
+        JE NO_ANS
+        JG ANSB
+        JL ANSA
     
+    CASE5: ;if B = C
+        MOV AL, B
+        CMP AL, A
+        JE NO_ANS
+        JG ANSA
+        JL ANSB
     
     ANSA:
     MOV AL, A
